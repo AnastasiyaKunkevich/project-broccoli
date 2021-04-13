a73591d3f33637dd326870ee0d24bbffe35d2a56
Anastasiya <example@gmail.com>
2021-04-13 11:24:07 +0300

    first commit

---
+++ 1/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt('How old are you?', '12');
+//console.log(answer+1);
+
+const user = 'Anastasiya';
+
+alert (`Hi!,${user}`);
